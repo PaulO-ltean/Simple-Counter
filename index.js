858ca932d59c18a192d23dd716a09b81c65c4b14
PaulO-ltean <oltean.paul.cristian@gmail.com>
2026-08-24 13:12:53 +0300

    Added "Sum of entries"

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let entrieEl = document.getElementById("entries-el")
 let count = 0
+let nrcounter = 0
 
 function increment() {
     count += 1
@@ -11,10 +13,14 @@ function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
+    nrcounter += count
+    entrieEl.textContent = "Sum of entries: " + nrcounter
     count = 0
 }
 
 function refresh()
 {
     saveEl.textContent = "Previous entries: "
+    entrieEl.textContent = "Sum of entries: "
+    nrcounter=0
 }
